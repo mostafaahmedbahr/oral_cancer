4df885d644b4b1e89e383bb18142080b7f0658fa
mostafa <mustafa.bahr12323@gmail.com>
2023-01-23 13:37:17 +0200

    j

--- lib/view/home/components/custom_drawer.dart
+++ lib/view/home/components/custom_drawer.dart
@@ -29,7 +29,7 @@ class CustomDrawer extends StatelessWidget {
               children: [
                 CircleAvatar(
                   radius: 50.r,
-                  backgroundImage: NetworkImage(
+                  backgroundImage: const NetworkImage(
                       "https://cdn.hswstatic.com/gif/play/0b7f4e9b-f59c-4024-9f06-b3dc12850ab7-1920-1080.jpg"),
                 ),
                 SizedBox(
